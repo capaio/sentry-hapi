71977c33bbe49230189fecd9d89a26132fddd469
Giovanni Cardamone <giovanni.cardamone@facile.it>
2022-09-28 17:32:13 +0200

    adding option for report conditionally 404

--- README.md
+++ README.md
@@ -1,27 +1,35 @@
 # Sentry-hapi plugin
 
+## 1. Install the plugin
 
-
-#### 1. Install the plugin
-```
+```bash
 npm install sentry-hapi
 ```
 
+## 2. create an environment variable called SENTRY_DSN
 
-#### 2. create an environment variable called SENTRY_DSN
-```
+```txt
 SENTRY_DSN=https://fake@sentry.yourdomain/
 ```
 
-#### 3. create an environment variable called ENV
-```
+## 3. create an environment variable called ENV
+
+```txt
 ENV=production
 ```
 
-#### 4. Include the plugin in your Hapi project
-```
+## 4. Include the plugin in your Hapi project
+
+```js
 await server.register({ plugin: require('sentry-hapi') });
 ```
 
+## 5. Configure options
 
-#### 5. Enjoy debugging your application!
+you can configure custom options of this plugin
+
+| Name      | Type    | Default | Description                  |
+|-----------|---------|---------|------------------------------|
+| report404 | Boolean | true    | send report of 404 happening |
+
+## 5. Enjoy debugging your application
--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@
 const Sentry = require('@sentry/node')
 Sentry.init({ dsn: process.env.SENTRY_DSN, environment: process.env.ENV })
 
-function sendSentry (request) {
+function sendSentry (request, options) {
   var res = request.response
+
+  console.log({report404: options.report404, statusCode: res.output.statusCode})
+
+  if (options.report404 === false && res.output.statusCode === 404) {
+    return // do not report 404 as requested in options
+  }
+
+  console.log('wewe')
+
+
   var req = request.raw.req
   var title = res.stack
   const extra = {
@@ -20,19 +30,28 @@ function sendSentry (request) {
     scope.setLevel('error')
     scope.setExtra('request', extra)
   })
+
   if(res.output.statusCode === 404) {
     title = 'Error: ' + request.url.href + ' not found';
   }
+
   Sentry.captureMessage(title);
 }
 
 exports.plugin = {
   name: 'Sentry-hapi',
   register: async function (server, options) {
+
+    // build options object
+    const finalOptions = Object.assign({
+      report404: true
+    }, options || {})
+
     server.ext('onPreResponse', function (request, reply) {
       if (request.response.isBoom) { // if there's an error
-        sendSentry(request)
+        sendSentry(request, finalOptions)
       }
+
       return reply.continue // continue processing the request
     })
   }
